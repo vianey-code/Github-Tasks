5c6df477ef65bb7f0511c82d7f704d528fd7359c
vianey <vianeynkem@gmail.com>
2022-04-03 21:39:29 +0100

    First C program

---
+++ hobkins/Codes.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main ()
+{                                                                         printf ("What's up my people! I am learning C language.\n");
+        return 0;
+}
